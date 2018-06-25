63472ac828300db13c1e64c57ff1f35ed11f8c2e
Paul Brauner <polux2001@gmail.com>
2018-06-25 13:28:46 +0200

    add garbage collection

--- src/Main.hs
+++ src/Main.hs
@@ -191,9 +191,11 @@ class MonadStore m a v | v -> a where
   ext :: a -> v -> m ()
   alloc :: Label -> m a
 
+type Store a v = M.Map a v
+
 {-- concrete stores --}
 
-type ConcreteStore = M.Map Int ConcreteValue
+type ConcreteStore = Store Int ConcreteValue
 
 asInt :: MonadError String m => ConcreteValue -> m Int
 asInt (VInt i) = return i
@@ -218,7 +220,7 @@ newtype History = History [Label]
 instance Show History where
   show (History xs) = "\"@" ++ intercalate "," (map show xs) ++ "\""
 
-type AbstractStore = M.Map History (S.Set AbstractValue)
+type AbstractStore = Store History (S.Set AbstractValue)
 
 instance MonadStore Abstract History AbstractValue where
   find a = do
@@ -237,8 +239,66 @@ instance MonadStore Abstract History AbstractValue where
     isAClosure (AClosure _ _ _) = True
     isAClosure _ = False
 
-  alloc l = Abstract . lift $ asks (cons l)
+  alloc l = Abstract . lift . lift $ asks (cons l)
     where cons l (History ls) = History (l:ls)
+
+{- Garbage Collection -}
+
+type Roots a = S.Set a
+
+class MonadGarbage m a where
+  askRoots :: m (Roots a)
+  extraRoots :: Roots a -> m b -> m b
+
+fv :: Ord n => Expr n l -> S.Set n
+fv (Lit _) = S.empty
+fv (Op2 _ a b) = fv a `S.union` fv b
+fv (IfZero a b c) = fv a `S.union` fv b `S.union` fv c
+fv (Var x) = S.singleton x
+fv (Lam x e) = S.delete x (fv e)
+fv (App _ a b) = fv a `S.union` fv b
+fv (Rec x e) = S.delete x (fv e)
+
+class HasRoots e a where
+  roots :: e -> Roots a
+
+exprRoots :: Ord a => LExpr -> Env a -> Roots a
+exprRoots e env = S.map (env M.!) (fv e)
+
+instance HasRoots ConcreteValue Int where
+  roots (VInt _) = S.empty
+  roots (VClosure env n e) = exprRoots (Lam n e) env
+
+instance HasRoots AbstractValue History where
+  roots (AInt _) = S.empty
+  roots (AClosure env n e) = exprRoots (Lam n e) env
+
+instance (Ord v, HasRoots a v) => HasRoots (S.Set a) v where
+  roots = S.unions . map roots . S.toList
+
+gc :: (Ord a, HasRoots v a) => Roots a -> Store a v -> Store a v
+gc addresses store = store `M.restrictKeys` reachable addresses addresses
+ where
+  reachable as seen | S.null as = seen
+                    | otherwise = reachable (as' S.\\ seen) (seen `S.union` as')
+    where as' = S.unions [ roots (store M.! a) | a <- S.toList as ]
+
+evGc
+  :: forall m a v av. ( MonadGarbage m a
+     , MonadState (M.Map a av) m
+     , Ord a
+     , HasRoots v a
+     , HasRoots av a
+     )
+  => ((LExpr -> m v) -> LExpr -> m v)
+  -> (LExpr -> m v)
+  -> LExpr
+  -> m v
+evGc ev0 ev e = do
+  rs <- askRoots @m @a
+  v <- ev0 ev e
+  modify (gc (S.union rs (roots v)))
+  return v
 
 {- Interpreter -}
 
@@ -247,7 +307,10 @@ ev
    . ( MonadError String m
      , MonadReader (Env a) m
      , MonadStore m a v
+     , MonadGarbage m a
      , Value m a v
+     , HasRoots v a
+     , Ord a
      )
   => (LExpr -> m v)
   -> LExpr
@@ -256,12 +319,16 @@ ev ev e = ev' e
  where
   ev' (Lit i) = toInt i
   ev' (Op2 op a b) = do
-    av <- ev a
-    bv <- ev b
+    env <- ask
+    av <- extraRoots (exprRoots b env) (ev a)
+    bv <- extraRoots (roots @v @a av) (ev b)
     op2 op av bv
   ev' (IfZero a b c) = do
-    isZeroA <- ev a >>= isZero
-    if isZeroA then ev b else ev c
+    env <- ask
+    let newRooots = S.union (exprRoots b env) (exprRoots c env)
+    av <- extraRoots newRooots (ev a)
+    isZeroA <- isZero av
+    ev (if isZeroA then b else c)
   ev' (Var x) = do
     env <- ask
     find (env M.! x)
@@ -269,45 +336,106 @@ ev ev e = ev' e
     env <- ask
     toClosure env x e
   ev' (App _ a b) = do
-    (env, x@(Name l _), e) <- ev a >>= asClosure @m @a @v
-    bv <- ev b
+    env <- ask
+    av <- extraRoots (exprRoots b env) (ev a)
+    bv <- extraRoots (roots @v @a av) (ev b)
+    (env, x@(Name l _), e) <- asClosure @m @a @v av
     addr <- alloc @m @a @v l
     ext addr bv
     local (const (M.insert x addr env)) (ev e)
   ev' (Rec f@(Name l _) e) = do
     env <- ask
     addr <- alloc @m @a @v l
-    ev <- local (M.insert f addr) (ev e)
-    ext addr ev
-    return ev
+    ve <- local (M.insert f addr) (ev e)
+    ext addr ve
+    return ve
 
 {- Concrete semantics -}
 
+type ConcreteStack =
+  ReaderT ConcreteEnv
+    (ReaderT (Roots Int)
+      (ExceptT String
+        (StateT ConcreteStore Identity)))
+
+newtype Concrete a = Concrete { runConcrete :: ConcreteStack a }
+  deriving
+    ( Functor
+    , Applicative
+    , Monad
+    , MonadReader ConcreteEnv
+    , MonadState ConcreteStore
+    , MonadError String)
+
+instance MonadGarbage Concrete Int where
+  askRoots = Concrete . lift $ ask
+  extraRoots roots a = Concrete $
+     liftControl $ \run ->
+       local (roots `S.union`) (run (runConcrete a))
+
 eval :: LExpr -> (Either String ConcreteValue, ConcreteStore)
 eval e =
-  runIdentity
-    $ flip runStateT M.empty    -- store
-    $ runExceptT                -- errors
-    $ flip runReaderT M.empty   -- environment
-    $ fix ev e
+  fix (evGc ev) e
+    & runConcrete
+    & flip runReaderT M.empty  -- environment
+    & flip runReaderT S.empty  -- garbage collection roots
+    & runExceptT               -- errors
+    & flip runStateT M.empty   -- store
+    & runIdentity
 
 {- Trace semantics -}
 
+type TracingStack =
+  ReaderT ConcreteEnv
+    (ReaderT (Roots Int)
+      (ExceptT String
+        (StateT ConcreteStore
+          (WriterT [ConcreteMachineState] Identity))))
+
+newtype Tracing a = Tracing { runTracing :: TracingStack a }
+  deriving
+    ( Functor
+    , Applicative
+    , Monad
+    , MonadReader ConcreteEnv
+    , MonadState ConcreteStore
+    , MonadWriter [ConcreteMachineState]
+    , MonadError String)
+
+instance MonadGarbage Tracing Int where
+  askRoots = Tracing . lift $ ask
+  extraRoots roots a = Tracing $
+     liftControl $ \run ->
+       local (roots `S.union`) (run (runTracing a))
+
 evTell ev0 ev e = do
-  v <- ev0 ev e
   env <- ask
+  roots <- askRoots
   store <- get
-  tell [(env, store, e, v)]
+  v <- ev0 ev e
+  tell [MachineState env roots store e v]
   return v
 
-type ConcreteMachineState = (Env Int, ConcreteStore, LExpr, ConcreteValue)
+data MachineState a v av = MachineState
+  { cmEnv :: Env a
+  , cmRoots :: Roots a
+  , cmStore :: Store a av
+  , cmExpr :: LExpr
+  , cmValue :: v
+  }
+  deriving (Ord, Eq, Show)
+
+type ConcreteMachineState = MachineState Int ConcreteValue ConcreteValue
+type AbstractMachineState = MachineState History AbstractValue (S.Set AbstractValue)
 
 evalCollect
   :: LExpr
   -> ((Either String ConcreteValue, ConcreteStore), [ConcreteMachineState])
 evalCollect e =
-  fix (evTell ev) e
+  fix (evGc (evTell ev)) e
+    & runTracing
     & flip runReaderT M.empty  -- environment
+    & flip runReaderT S.empty  -- garbage collection roots
     & runExceptT               -- errors
     & flip runStateT M.empty   -- store
     & runWriterT               -- collected states
@@ -323,10 +451,10 @@ class MonadHistory m where
 evHistory
   :: MonadHistory m
   => Int
-  -> ((LExpr -> m mValue) -> LExpr -> m mValue)
-  -> (LExpr -> m mValue)
+  -> ((LExpr -> m v) -> LExpr -> m v)
+  -> (LExpr -> m v)
   -> LExpr
-  -> m mValue
+  -> m v
 evHistory limit ev0 ev e
   | App l _ _ <- e = localHistory (extendHistory l) (ev0 ev e)
   | otherwise = ev0 ev e
@@ -336,7 +464,7 @@ evHistory limit ev0 ev e
 
 {-- cache --}
 
-type Configuration = (LExpr, AbstractEnv, AbstractStore)
+type Configuration = (LExpr, AbstractEnv, Roots History, AbstractStore)
 type ValueAndStore = (AbstractValue, AbstractStore)
 type Cache = M.Map Configuration (S.Set ValueAndStore)
 
@@ -356,6 +484,7 @@ modifyCacheOut f = do
 evCache
   :: ( MonadReader AbstractEnv m
      , MonadState AbstractStore m
+     , MonadGarbage m History
      , MonadCache m
      , MonadPlus m
      )
@@ -365,8 +494,9 @@ evCache
   -> m AbstractValue
 evCache ev0 ev e = do
   env <- ask
+  roots <- askRoots
   store <- get
-  let config = (e, env, store)
+  let config = (e, env, roots, store)
   cacheOut <- getCacheOut
   case M.lookup config cacheOut of
     Just entries -> do
@@ -385,6 +515,7 @@ evCache ev0 ev e = do
 fixCache
   :: ( MonadReader AbstractEnv m
      , MonadState AbstractStore m
+     , MonadGarbage m History
      , MonadCache m
      , MonadPlus m
      )
@@ -393,8 +524,9 @@ fixCache
   -> m AbstractValue
 fixCache ev e = do
   env <- ask
+  roots <- askRoots
   store <- get
-  let config = (e, env, store)
+  let config = (e, env, roots, store)
   cachePlus <- mlfp M.empty $ \cache -> do
     putCacheOut M.empty
     put store
@@ -419,12 +551,14 @@ mlfp bot f = loop bot
 
 type AbstractStack =
   ReaderT AbstractEnv
-    (ReaderT History
-      (ExceptT String
-        (StateT AbstractStore
-          (ListT
-            (ReaderT Cache
-              (StateT Cache Identity))))))
+    (ReaderT (Roots History)
+      (ReaderT History
+        (ExceptT String
+          (WriterT [AbstractMachineState]
+            (StateT AbstractStore
+              (ListT
+                (ReaderT Cache
+                  (StateT Cache Identity))))))))
 
 newtype Abstract a = Abstract { runAbstract :: AbstractStack a }
   deriving
@@ -433,7 +567,8 @@ newtype Abstract a = Abstract { runAbstract :: AbstractStack a }
     , Monad
     , MonadReader AbstractEnv
     , MonadState AbstractStore
-    , MonadError String)
+    , MonadError String
+    , MonadWriter [AbstractMachineState])
 
 liftControl
   :: (MonadTransControl t, Monad m, Monad (t m))
@@ -444,37 +579,54 @@ liftControl f = liftWith f >>= restoreT . return
 -- We can't rely on GeneralizedNewtypeDeriving for the Alternative instance
 -- because it would select the ExceptT instance instead of the ListT one.
 instance Alternative Abstract where
-  empty = Abstract . lift . lift . lift $ empty
+  empty = Abstract . lift . lift . lift . lift . lift . lift $ empty
   Abstract a <|> Abstract b = Abstract $
-    liftControl $ \run1 ->     -- peel ReaderT
-      liftControl $ \run2 ->   -- peel ExceptT
-        liftControl $ \run3 -> -- peel SateT
-          (run3 . run2 . run1) a <|> (run3 . run2 . run1) b
+    liftControl $ \run1 ->           -- peel ReaderT AbstractEnv
+      liftControl $ \run2 ->         -- peel ReaderT (Roots History)
+        liftControl $ \run3 ->       -- peel ReaderT History
+          liftControl $ \run4 ->     -- peel ExceptT String
+            liftControl $ \run5 ->   -- peel WriterT [AbstractMachineState]
+              liftControl $ \run6 -> -- peel StateT AbstractStore
+                let run = run6 . run5 . run4 . run3 . run2 . run1
+                in run a <|> run b
 
 instance MonadPlus Abstract
 
 instance MonadCache Abstract where
-  getCacheIn = Abstract . lift . lift . lift . lift $ ask
+  getCacheIn = Abstract . lift . lift . lift . lift . lift . lift . lift $ ask
   withLocalCacheIn c (Abstract m) = Abstract $
     liftControl $ \run1 ->
       liftControl $ \run2 ->
         liftControl $ \run3 ->
           liftControl $ \run4 ->
-            local (const c) (run4 . run3 . run2 . run1 $ m)
-  getCacheOut = Abstract . lift . lift . lift . lift . lift $ get
-  putCacheOut c = Abstract . lift . lift . lift . lift . lift $ put c
+            liftControl $ \run5 ->
+              liftControl $ \run6 ->
+                liftControl $ \run7 ->
+              local (const c) (run7 . run6 . run5 . run4 . run3 . run2 . run1 $ m)
+  getCacheOut = Abstract . lift . lift . lift . lift . lift . lift . lift . lift $ get
+  putCacheOut c = Abstract . lift . lift . lift . lift . lift . lift . lift . lift $ put c
 
 instance MonadHistory Abstract where
-  localHistory f a =
-    Abstract $ liftControl $ \run -> local f (run (runAbstract a))
+  localHistory f a = Abstract $
+     liftControl $ \run1 ->
+       liftControl $ \run2 ->
+         local f ((run2 . run1) (runAbstract a))
 
-evalAbstract :: Int -> LExpr -> ([(Either String AbstractValue, AbstractStore)], Cache)
+instance MonadGarbage Abstract History where
+  askRoots = Abstract . lift $ ask
+  extraRoots roots a = Abstract $
+     liftControl $ \run ->
+       local (roots `S.union`) (run (runAbstract a))
+
+evalAbstract :: Int -> LExpr -> ([((Either String AbstractValue, [AbstractMachineState]), AbstractStore)], Cache)
 evalAbstract limit e =
-  fixCache (fix (evCache (evHistory limit ev))) e
+  fixCache (fix (evCache (evHistory limit (evGc (evTell ev))))) e
     & runAbstract
     & flip runReaderT M.empty      -- environment
+    & flip runReaderT S.empty      -- garbage collection roots
     & flip runReaderT (History []) -- history
     & runExceptT                   -- errors
+    & runWriterT                   -- trace
     & flip runStateT M.empty       -- store
     & runListT                     -- non-determinism
     & flip runReaderT M.empty      -- cache-in
@@ -494,6 +646,7 @@ a /: b = Op2 Div a b
 
 letIn x e1 e2 = Lam x e2 @: e1
 
+fact :: UExpr
 fact =
   Rec "fact" (Lam "n" (IfZero "n" (Lit 1) ("n" *: ("fact" @: ("n" -: Lit 1)))))
 
@@ -506,10 +659,10 @@ main = do
   pPrint $ eval ltest
   printHeader "-- CONCRETE TRACE --"
   pPrint $ evalCollect ltest
-  printHeader "-- ABSTRACT 3 --"
-  pPrint $ S.fromList $ fst $ evalAbstract 3 ltest
-  printHeader "-- ABSTRACT 6 --"
-  pPrint $ S.fromList $ fst $ evalAbstract 6 ltest
+  printHeader "-- ABSTRACT 1 --"
+  pPrint $ S.fromList $ fst $ evalAbstract 1 ltest
+  printHeader "-- ABSTRACT 2 --"
+  pPrint $ S.fromList $ fst $ evalAbstract 2 ltest
 
   where printHeader s = putStrLn ("\n" ++ s ++ "\n")
         ltest = label test
